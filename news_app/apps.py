ab7c3b2f5c4e2324aa72feb1602b654815e72ced
nandi-y <2209ynd@gmail.com>
2025-09-02 00:24:46 +0200

    Add docstring to NewsAppConfig in apps.py

--- news_app/apps.py
+++ news_app/apps.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 
 class NewsAppConfig(AppConfig):
+    """
+    Django AppConfig for the news_app application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_app'
 
